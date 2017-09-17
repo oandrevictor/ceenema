f550d44dcf2feaa5c39f4956b7f06a5cfc640bba
André Andrade <oandrevictor@gmail.com>
2017-09-17 12:57:46 -0300

    Heroku checked. Favicon add.

--- app/assets/javascripts/fixed.js
+++ app/assets/javascripts/fixed.js
@@ -4,9 +4,7 @@ $(document).ready(function() {
         //if you hard code, then use console
         //.log to determine when you want the
         //nav bar to stick.
-        console.log($(window).scrollTop())
         if ($(window).scrollTop() > 2*($('#nav_bar').height())) {
-            console.log("eai")
             $('#nav_bar').addClass('navbar-fixed');
         }
         if ($(window).scrollTop() < $('#nav_bar').height() +1) {
